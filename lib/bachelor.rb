8c8d9308451bffaf3ba0ca4a1bc09171855227a5
Nicole Dow <nikki.dow71@gmail.com>
2019-05-14 22:13:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -51,8 +51,8 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  data.each |season, array_of_contestants|
-    if season.to_s == season
+  data.each |season_title, array_of_contestants|
+    if season_title.to_s == season
       array_of_contestants.each do |contestant|
         binding.pry
       end
