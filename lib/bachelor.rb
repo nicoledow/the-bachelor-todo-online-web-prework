6a48f25fde613902b76919d91d238cd8e1434429
Nicole Dow <nikki.dow71@gmail.com>
2019-05-14 22:10:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -41,7 +41,11 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  
+  data.each do |season, array_of_contestants|
+    array_of_contestants.each do |contestant|
+      binding.pry
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
