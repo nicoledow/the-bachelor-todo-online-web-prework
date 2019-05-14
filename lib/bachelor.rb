cf4709239eb39334562cb22c96bd2b137d62e900
Nicole Dow <nikki.dow71@gmail.com>
2019-05-14 21:23:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,10 @@ require "pry"
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season, contestant_list|
-    contestant_list.each do |contestant, info|
-      binding.pry
+    contestant_list.each do |contestant, attribute|
+      contestant.each do |attribute, info|
+        binding.pry
+      end
     end
   end
 end
