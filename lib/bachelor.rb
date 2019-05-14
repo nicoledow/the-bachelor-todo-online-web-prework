f38bcb3164b7c5616bfa0769dbb077a5864a0ff6
Nicole Dow <nikki.dow71@gmail.com>
2019-05-14 21:44:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,7 @@ def get_first_name_of_season_winner(data, season)
     if season.to_s == season
       list_of_contestants.each do |contestant_data_set, data|
         binding.pry
-        if contestant_data_set[:status] == "Winner"
+        if contestant_data_set["status"] == "Winner"
           full_name_arr = data[season][contestant_data_set]["name"].split(" ")
           first_name = full_name_arr[0]
           return first_name
